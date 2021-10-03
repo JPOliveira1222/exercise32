227d6b92cd5c72b28496ec8310f672988c4d0d81
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-03 18:49:34 -0400

    pseudocode for 32

---
+++ src/main/java/exercise32/solution32.java
@@ -0,0 +1,32 @@
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 32 Solution
+ *  Copyright 2021 Joao Pedro Oliveira
+ */
+
+package exercise32;
+import java.util.*;
+
+
+public class solution32 {
+
+    /*
+    Print "Let's play Guess The Number!"
+
+    initialize a number and set it to random number between 1-1000
+
+    ask what the difficulty the user would like to play
+    if the difficulty is 1 get a random number between 1-10
+    if the difficulty is 2 get a random number between 1-100
+    if the difficulty is 3 get a random number between 1-1000
+
+    have the user guess until they get the correct random number
+    If the user guess is too low "Too low. Guess again."
+    if the user guess is too high "Too high. Guess again."
+    if the user guess is correct print "you got it in " +total+ "Guesses"
+
+     */
+
+    
+
+}
